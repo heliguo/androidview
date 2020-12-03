8f173471ebe52ada5447ac41fecd8499c57cfa2d
lgh <ysulgh@163.com>
2020-12-03 10:25:44 +0800

    ViewFlippers

--- app/src/main/java/com/example/androidview/ViewFlipperActivity.java
+++ app/src/main/java/com/example/androidview/ViewFlipperActivity.java
@@ -1,11 +1,18 @@
 package com.example.androidview;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Stack;
 
 /**
  * @author lgh
@@ -15,7 +22,31 @@ public class ViewFlipperActivity extends AppCompatActivity {
 
     private static final String TAG = "ViewFlipperActivity";
 
-    ViewFlipper mViewFlipper;
+    ViewFlippers mViewFlipper;
+
+    Stack<String> mStack = new Stack<>();
+
+    private Handler mHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+
+            if (msg.what == 1) {
+                View view = LayoutInflater.from(ViewFlipperActivity.this).inflate(R.layout.item_flipper_text, null);
+                TextView textView = view.findViewById(R.id.task_flipper_text);
+                textView.setText(mStack.pop());
+                if (mStack.size() <= 2) {
+                    randomContents();
+                }
+                mViewFlipper.addView(view);
+                if (!mViewFlipper.isFlipping()) {
+                    mViewFlipper.setFlipInterval(2000);
+                    mViewFlipper.startFlipping();
+                }
+                mHandler.sendEmptyMessageDelayed(1, 2000);
+            }
+            return false;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +57,21 @@ public class ViewFlipperActivity extends AppCompatActivity {
 
     private void initView() {
         mViewFlipper = findViewById(R.id.view_filpper);
-        int a = 3;
-        for (int i = 0; i < a; i++) {
-            final int position = i;
-            View view = getLayoutInflater().inflate(R.layout.item_viewflipper, null);
-            view.setOnClickListener(v -> Log.e(TAG, "onClick: " + position));
-            mViewFlipper.addView(view);
-        }
+        mStack.push("1");
+        mStack.push("2");
+        mStack.push("3");
+        mStack.push("4");
+        mStack.push("5");
+//        mHandler.sendEmptyMessage(1);
         mViewFlipper.setFlipInterval(2000);
         mViewFlipper.startFlipping();
+    }
+
+    private void randomContents() {
+        mStack.push("8888");
+        mStack.push("9999");
+        mStack.push("6666");
+        View view = mViewFlipper.getCurrentView();
     }
 
     @Override
@@ -70,4 +107,12 @@ public class ViewFlipperActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mHandler != null) {
+            mHandler.removeMessages(1);
+            mHandler = null;
+        }
+    }
 }
