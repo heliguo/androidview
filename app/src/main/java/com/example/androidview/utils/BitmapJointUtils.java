8ae00b2bca845829604aebf99702ac0968e15705
lgh <ysulgh@163.com>
2021-03-10 11:14:28 +0800

    view、scrollview、recyclerview、list view 转bitmap

--- app/src/main/java/com/example/androidview/utils/BitmapJointUtils.java
+++ app/src/main/java/com/example/androidview/utils/BitmapJointUtils.java
@@ -2,9 +2,14 @@ package com.example.androidview.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.LruCache;
 import android.view.View;
 
 import androidx.annotation.ColorInt;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,11 +33,17 @@ public class BitmapJointUtils {
         }
         List<Bitmap> bitmaps = new ArrayList<>();
         for (View view : showView) {
-            view.setDrawingCacheEnabled(true);
-            view.buildDrawingCache();
-            bitmaps.add(view.getDrawingCache());
+            if (view instanceof RecyclerView) {
+                Bitmap bitmap = shotRecyclerView(((RecyclerView) view));
+                bitmaps.add(bitmap);
+            }else {
+                view.setDrawingCacheEnabled(true);
+                view.buildDrawingCache();
+                bitmaps.add(view.getDrawingCache());
+            }
+
         }
-        if (hasHide){
+        if (hasHide) {
             for (View view : hideView) {
                 view.setVisibility(View.VISIBLE);
             }
@@ -53,16 +64,71 @@ public class BitmapJointUtils {
         }
         List<Bitmap> bitmaps = new ArrayList<>();
         for (View view : showView) {
-            view.setDrawingCacheEnabled(true);
-            view.buildDrawingCache();
-            bitmaps.add(view.getDrawingCache());
+            if (view instanceof RecyclerView) {
+                Bitmap bitmap = shotRecyclerView(((RecyclerView) view));
+                bitmaps.add(bitmap);
+            } else {
+                view.setDrawingCacheEnabled(true);
+                view.buildDrawingCache();
+                bitmaps.add(view.getDrawingCache());
+            }
+
         }
-        if (hasHide){
+        if (hasHide) {
             for (View view : hideView) {
                 view.setVisibility(View.VISIBLE);
             }
         }
         return mosaicBitmapHorizontal(bgColor, bitmaps);
+    }
+
+    private static Bitmap shotRecyclerView(RecyclerView view) {
+        RecyclerView.Adapter adapter = view.getAdapter();
+        Bitmap bigBitmap = null;
+        if (adapter != null) {
+            int size = adapter.getItemCount();
+            int height = 0;
+            Paint paint = new Paint();
+            int iHeight = 0;
+            final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+            // Use 1/8th of the available memory for this memory cache.
+            final int cacheSize = maxMemory/8;
+            LruCache<String, Bitmap> bitmapCache = new LruCache<>(cacheSize);
+            for (int i = 0; i < size; i++) {
+                RecyclerView.ViewHolder holder = adapter.createViewHolder(view, adapter.getItemViewType(i));
+                adapter.onBindViewHolder(holder, i);
+                holder.itemView.measure(
+                        View.MeasureSpec.makeMeasureSpec(view.getWidth(), View.MeasureSpec.EXACTLY),
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+                holder.itemView.layout(0, 0, holder.itemView.getMeasuredWidth(),
+                        holder.itemView.getMeasuredHeight());
+                holder.itemView.setDrawingCacheEnabled(true);
+                holder.itemView.buildDrawingCache();
+                Bitmap drawingCache = holder.itemView.getDrawingCache();
+                if (drawingCache != null) {
+                    bitmapCache.put(String.valueOf(i), drawingCache);
+                }
+                height += holder.itemView.getMeasuredHeight();
+            }
+
+            bigBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), height, Bitmap.Config.ARGB_8888);
+            Canvas bigCanvas = new Canvas(bigBitmap);
+            Drawable lBackground = view.getBackground();
+            if (lBackground instanceof ColorDrawable) {
+                ColorDrawable lColorDrawable = (ColorDrawable) lBackground;
+                int lColor = lColorDrawable.getColor();
+                bigCanvas.drawColor(lColor);
+            }
+
+            for (int i = 0; i < size; i++) {
+                Bitmap bitmap = bitmapCache.get(String.valueOf(i));
+                bigCanvas.drawBitmap(bitmap, 0f, iHeight, paint);
+                iHeight += bitmap.getHeight();
+                bitmap.recycle();
+            }
+        }
+        return bigBitmap;
     }
 
 
