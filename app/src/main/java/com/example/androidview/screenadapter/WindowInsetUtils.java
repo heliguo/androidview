48730256fc8dcfaba605d468abaa25db61951040
lgh <ysulgh@163.com>
2021-04-25 16:20:08 +0800

    刘海屏

--- app/src/main/java/com/example/androidview/screenadapter/WindowInsetUtils.java
+++ app/src/main/java/com/example/androidview/screenadapter/WindowInsetUtils.java
@@ -30,8 +30,8 @@ public class WindowInsetUtils {
     }
 
     public static void setFullScreen(Window window) {
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if (hasWindInsets(window)) {
-            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
             /*
              * 处理刘海屏
              * #LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT 全屏模式下，内容下移，非全屏不受影响
@@ -44,14 +44,14 @@ public class WindowInsetUtils {
                 window.setAttributes(params);
             }
 
-            window.getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE //来帮助你的app来维持一个稳定的布局
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //确保appUI的主要部分不会因为被系统导航栏覆盖而结束
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN //确保appUI的主要部分不会因为被系统状态栏覆盖而结束
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //隐藏导航栏
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN //表示全屏，会将状态栏隐藏，只会隐藏状态栏
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);//粘性沉浸
         }
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE //来帮助你的app来维持一个稳定的布局
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //确保appUI的主要部分不会因为被系统导航栏覆盖而结束
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN //确保appUI的主要部分不会因为被系统状态栏覆盖而结束
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION //隐藏导航栏
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN //表示全屏，会将状态栏隐藏，只会隐藏状态栏
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);//粘性沉浸
 
     }
 
